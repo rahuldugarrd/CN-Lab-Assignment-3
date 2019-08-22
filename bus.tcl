40e101b4f3dccbbecd5432c14f4cf38faa14f4e4
rahuldugar <rahuldugarrd@gmail.com>
2019-08-22 11:27:32 +0530

    correction

--- bus.tcl
+++ bus.tcl
@@ -1,18 +1,18 @@
 set data [gets stdin]
 scan $data "%d %d" N k
+
 set ns  [new Simulator]
-$ns rtproto DV
+
 set nf [open out.nam w]
 $ns namtrace-all $nf
 
 proc finish {} {
-    global ns nf
-    $ns flush-trace
-    close $nf
-    exec nam out.nam
-    exit 0
-}
-
+        global ns nf
+        $ns flush-trace
+        close $nf
+        exec nam out.nam
+        exit 0
+        }
 $ns color 0 red
 $ns color 3 orange
 $ns color 2 purple
@@ -26,6 +26,7 @@ $ns color 11 snow
 $ns color 7 aqua
 $ns color 9 azure
 
+#creating Nodes        
 set nodes ""
 for {set i 0} {$i < $N} {incr i} {
 	set n($i) [$ns node]
@@ -37,30 +38,18 @@ for {set i 0} {$i < $N} {incr i} {
 	}
 }
 puts $nodes
-set originX 0
-set originY 0
-set width   100
-set height  100
-
-set rowsize [expr {$height / 4}]
-set rowY [expr {$originY + $rowsize / 2}]
-set colsize [expr {$width / $N}]
-for {set i 0} {$i < $N} {incr i} {
-    set rowX [expr {$originX + $i * $colsize}]
-    set rowY [expr {$originY + ( $i % 2 ) * $rowsize}]
-    $n($i) set X_ $rowX
-    $n($i) set Y_ $rowY
-}
 
 set lan0 [$ns newLan $nodes 0.5Mb 40ms LL Queue/DropTail MAC/Csma/Cd Channel]
+
+# TCP_Config
 
 for {set i 0} {$i < $k} {incr i} {
 	set input [gets stdin]
 	scan $input "%d %d" u v
-	set tcp [new Agent/TCP]
+	set tcp [new Agent/TCP/Newreno]
 	$ns attach-agent $n($u) $tcp
 	$tcp set class_ $i
-	set sink [new Agent/TCPSink]
+	set sink [new Agent/TCPSink/DelAck]
 	$ns attach-agent $n($v) $sink
 	$ns connect $tcp $sink
 	set ftp0 [new Application/FTP]
